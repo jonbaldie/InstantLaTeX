b44bdff5a8a2296b4d7a28eb6713bb35e6398bb9
Jonathan Baldie <jon@jonbaldie.com>
2026-05-07 10:25:55 +0100

    chore: Decouple Google Analytics from main.js

--- public_html/main.js
+++ public_html/main.js
@@ -3,13 +3,6 @@ function formatMath(TeX) {
 }
 
 if (typeof window !== 'undefined') {
-    (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-    (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-    m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-    })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-    ga('create', 'UA-54119971-32', 'auto');
-    ga('send', 'pageview');
-
     (function (w) {
         w.UpdateMath = function (TeX) {
             const arg = formatMath(TeX);
